2a5f92eca9d27e9a200cc414b8774dd95ea7cf2c
rwparrish <60946750+rwparrish@users.noreply.github.com>
2022-07-14 13:27:53 -0600

    done son

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
-
+import ipdb
+ 
+ 
 def print_fibonacci(length):
-    pass
+    fib_sequence = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+    i = 0
+    if length == 0:
+        print('', end="")
+    while length > 0:
+        print(fib_sequence[i])
+        i += 1
+        length -= 1
+        
+    
+    
+    
+    
+    
+        
